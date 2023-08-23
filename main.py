806ff90cd91d388054fa1be3809a413353b667e6
samanta-sumit <111765256+samanta-sumit@users.noreply.github.com>
2023-08-23 17:57:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+def sum(a ,b ,c):
+   return a+b+c
+
+def printBoard(xstate,ystate):
+    zero='x' if xstate[0] else ('0' if ystate[0] else 0)
+    one='x' if xstate[1] else ('0' if ystate[1] else 1)
+    two='x' if xstate[2] else ('0' if ystate[2] else 2)
+    three='x' if xstate[3] else ('0' if ystate[3] else 3)
+    four='x' if xstate[4] else ('0' if ystate[4] else 4)
+    five='x' if xstate[5] else ('0' if ystate[5] else 5)
+    six='x' if xstate[6] else ('0' if ystate[6] else 6)
+    seven='x' if xstate[7] else ('0' if ystate[7] else 7)
+    eight='x' if xstate[8] else ('0' if ystate[8] else 8)
+    print(f" {zero} | {one} | {two}")
+    print(f"---|---|--")
+    print(f" {three} | {four} | {five}")
+    print(f"---|---|--")
+    print(f" {six} | {seven} | {eight}")
+
+def checkWin(xstate, ystate):
+   win=[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+   for win in win:
+        if (sum(xstate[win[0]], xstate[win[1]], xstate[win[2]])==3):
+          print("x wins ")
+          return 1
+        
+        if(sum(ystate[win[0]], ystate[win[1]], ystate[win[2]])==3):
+           print("o wins ")
+           return 0
+       
+   return -1
+
+xstate=[0,0,0,0,0,0,0,0,0]
+ystate=[0,0,0,0,0,0,0,0,0]
+turn=1
+print("welcom to the game")
+
+while(True):
+    printBoard(xstate,ystate)
+    if(turn==1):
+      print("x's chance")
+      value=int(input("plz enter a value "))
+      xstate[value]=1
+    else:
+       print("y's chance")
+       value=int(input("plz enter a value "))
+       ystate[value]=1
+
+    cwin=checkWin(xstate, ystate)
+    if(cwin!= -1):
+       print("match over")
+       break
+
+    turn = 1-turn
